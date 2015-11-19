40f0fa4e956f690ff54866b948fbcb75e29a62fc
Spencer Comerford <Spencer@Spen.co>
2015-11-18 17:42:44 -0700

    Update simian.rb

--- Library/Formula/simian.rb
+++ Library/Formula/simian.rb
@@ -3,7 +3,6 @@ require 'formula'
 class Simian < Formula
   url 'http://www.harukizaemon.com/simian/simian-2.4.0.tar.gz'
   homepage 'http://www.harukizaemon.com/simian/index.html'
-  md5 'cbe85953e518a376d2a3fe1f47d908c3'
 
   def install
     libexec.install "bin/simian-2.4.0.jar"
